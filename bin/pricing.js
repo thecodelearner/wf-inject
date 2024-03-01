75a17f4f637a305c905aa57b41fe07e35a52f043
Sahil Sanghavi <22259087+thecodelearner@users.noreply.github.com>
2024-03-01 21:05:19 +0530

    update pricing.js

--- bin/pricing.js
+++ bin/pricing.js
@@ -50,52 +50,52 @@ function handleTabSwitch(sw_btn) {
 
     if (termCb.checked) {
       base_amt_span.forEach((amt) => {
-        amt.innerHTML = "9";
+        amt.textContent = "9";
       });
       pro_amt_span.forEach((amt) => {
-        amt.innerHTML = "60";
+        amt.textContent = "60";
       });
       promax_amt_span.forEach((amt) => {
-        amt.innerHTML = "120";
+        amt.textContent = "120";
       });
     } else {
       base_amt_span.forEach((amt) => {
-        amt.innerHTML = "9";
+        amt.textContent = "9";
       });
       pro_amt_span.forEach((amt) => {
-        amt.innerHTML = "50";
+        amt.textContent = "50";
       });
       promax_amt_span.forEach((amt) => {
-        amt.innerHTML = "100";
+        amt.textContent = "100";
       });
     }
 
     base_subtext.forEach((price) => {
-      price.innerHTML = "for 3 agents/month";
+      price.textContent = "for 3 agents/month";
     });
     pro_subtext.forEach((price) => {
-      price.innerHTML = "per agent/month";
+      price.textContent = "per agent/month";
     });
     promax_subtext.forEach((price) => {
-      price.innerHTML = "per agent/month";
+      price.textContent = "per agent/month";
     });
 
     product_para.forEach((para) => {
-      para.innerHTML = "AI-powered Support Inbox";
+      para.textContent = "AI-powered Support Inbox";
     });
 
     base_para.forEach((para) => {
-      para.innerHTML =
+      para.textContent =
         'Alternative to <strong class="bold-text">Shopify Inbox.</strong><br><span class="span-top-mg">Manage Email, Social and Chat from one app.</span>';
     });
 
     pro_para.forEach((para) => {
-      para.innerHTML =
+      para.textContent =
         'Alternative to <strong class="bold-text">___.</strong><br><span class="span-top-mg">All features, automations &amp; AI with a fair price.</span>';
     });
 
     promax_para.forEach((para) => {
-      para.innerHTML =
+      para.textContent =
         'Alternative to <strong class="bold-text">Zendesk.</strong><br><span class="span-top-mg">Powerful platform, is easy &amp; has great support.</span>';
     });
 
@@ -109,53 +109,53 @@ function handleTabSwitch(sw_btn) {
 
     if (termCb.checked) {
       base_amt_span.forEach((amt) => {
-        amt.innerHTML = "0";
+        amt.textContent = "0";
       });
       pro_amt_span.forEach((amt) => {
-        amt.innerHTML = "100";
+        amt.textContent = "100";
       });
       promax_amt_span.forEach((amt) => {
-        amt.innerHTML = "300";
+        amt.textContent = "300";
       });
     } else {
       base_amt_span.forEach((amt) => {
-        amt.innerHTML = "0";
+        amt.textContent = "0";
       });
       pro_amt_span.forEach((amt) => {
-        amt.innerHTML = "120";
+        amt.textContent = "120";
       });
       promax_amt_span.forEach((amt) => {
-        amt.innerHTML = "360";
+        amt.textContent = "360";
       });
     }
 
     base_subtext.forEach((price) => {
-      price.innerHTML = "Free add-on&nbsp;<br>&nbsp;";
+      price.textContent = "Free add-on&nbsp;<br>&nbsp;";
     });
     pro_subtext.forEach((price) => {
-      price.innerHTML = "/month";
+      price.textContent = "/month";
     });
     promax_subtext.forEach((price) => {
-      price.innerHTML = "/month";
+      price.textContent = "/month";
     });
 
     product_para.forEach((para) => {
-      para.innerHTML =
+      para.textContent =
         "Automate upto 50% of your inquires. Add-on to your Help Desk plan.";
     });
 
     base_para.forEach((para) => {
-      para.innerHTML =
+      para.textContent =
         'Alternative to <strong class="bold-text">FAQ&nbsp;Apps.</strong><br><span class="span-top-mg">With order tracking.<br></span>';
     });
 
     pro_para.forEach((para) => {
-      para.innerHTML =
+      para.textContent =
         'Alternative to <strong class="bold-text">Gorgias.</strong><br><span class="span-top-mg">Unlimited resolutions.</span>';
     });
 
     promax_para.forEach((para) => {
-      para.innerHTML =
+      para.textContent =
         'Alternative to <strong class="bold-text">Chatbots.</strong><br><span class="span-top-mg">Easier to use with multiple languages.</span>';
     });
 
@@ -203,23 +203,23 @@ function handleTermChange() {
   if (activeTab == "helpdesk") {
     if (termCb.checked) {
       base_amt_span.forEach((amt) => {
-        amt.innerHTML = "9";
+        amt.textContent = "9";
       });
       pro_amt_span.forEach((amt) => {
-        amt.innerHTML = "50";
+        amt.textContent = "50";
       });
       promax_amt_span.forEach((amt) => {
-        amt.innerHTML = "100";
+        amt.textContent = "100";
       });
     } else {
       base_amt_span.forEach((amt) => {
-        amt.innerHTML = "9";
+        amt.textContent = "9";
       });
       pro_amt_span.forEach((amt) => {
-        amt.innerHTML = "60";
+        amt.textContent = "60";
       });
       promax_amt_span.forEach((amt) => {
-        amt.innerHTML = "120";
+        amt.textContent = "120";
       });
     }
   }
@@ -227,23 +227,23 @@ function handleTermChange() {
   if (activeTab == "customerportal") {
     if (termCb.checked) {
       base_amt_span.forEach((amt) => {
-        amt.innerHTML = "0";
+        amt.textContent = "0";
       });
       pro_amt_span.forEach((amt) => {
-        amt.innerHTML = "100";
+        amt.textContent = "100";
       });
       promax_amt_span.forEach((amt) => {
-        amt.innerHTML = "300";
+        amt.textContent = "300";
       });
     } else {
       base_amt_span.forEach((amt) => {
-        amt.innerHTML = "0";
+        amt.textContent = "0";
       });
       pro_amt_span.forEach((amt) => {
-        amt.innerHTML = "120";
+        amt.textContent = "120";
       });
       promax_amt_span.forEach((amt) => {
-        amt.innerHTML = "360";
+        amt.textContent = "360";
       });
     }
   }
@@ -265,18 +265,15 @@ function showStickyWrapper() {
     stickyWrapper.style.display = "block";
     stickyWrapper.style.position = "fixed";
     stickyWrapper.style.opacity = "100";
-    //console.log("scrolled >=192")
   } else if (document.documentElement.scrollTop <= 279) {
     stickyWrapper.style.display = "none";
     stickyWrapper.style.opacity = "0";
-    //console.log("---scrolled <=382")
   } else if (
     document.documentElement.scrollTop >=
     heightOffset + tabcHeight + 1
   ) {
     stickyWrapper.style.display = "none";
     stickyWrapper.style.opacity = "0";
-    //console.log("---scrolled >=3100");
   }
 }
 
